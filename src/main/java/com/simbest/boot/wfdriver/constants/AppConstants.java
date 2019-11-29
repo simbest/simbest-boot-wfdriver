60383747865d6920334d0029f181b71e30f4d77b
kyne0116 <kyne.li@hotmail.com>
2019-11-29 15:41:04 +0800

    init project

---
+++ src/main/java/com/simbest/boot/wfdriver/constants/AppConstants.java
@@ -0,0 +1,22 @@
+package com.simbest.boot.wfdriver.constants;
+
+/**
+ * <strong>Title : AppConstants</strong><br>
+ * <strong>Description : 常量工具类</strong><br>
+ * <strong>Create on : 2018/8/21</strong><br>
+ * <strong>Modify on : 2018/8/21</strong><br>
+ * <strong>Copyright (C) Ltd.</strong><br>
+ *
+ * @author LJW lijianwu@simbest.com.cn
+ * @version <strong>V1.0.0</strong><br>
+ * <strong>修改历史:</strong><br>
+ * 修改人 修改日期 修改描述<br>
+ * -------------------------------------------<br>
+ */
+public final class AppConstants {
+
+    private AppConstants(){}
+
+    public final static String APP_CODE = "wfdriver";
+
+}
